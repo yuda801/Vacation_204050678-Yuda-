588ca79c0158770f664885da07f462c3eb952d12
YUDA RECHT <yudar801@gmail.com>
2022-04-25 05:33:57 +0300

    made cards

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -24,26 +24,29 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-
-// const ExpandMore = styled((props) => {
-//     const { expand, ...other } = props;
-//     return <IconButton {...other} />;
-//   })(({ theme, expand }) => ({
-//     transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-//     marginLeft: 'auto',
-//     transition: theme.transitions.create('transform', {
-//       duration: theme.transitions.duration.shortest,
-//     }),
-//   }));
+import Button from '@mui/material/Button';
 
 
-//   const handleExpandClick = () => {
-//       setExpanded(!expanded);
-//     };
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
+
+
+
 
 
 const Home = (props) => {
     const [expanded, setExpanded] = React.useState(false);
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
     console.log("home-page")
 
     let navigate = useNavigate();
@@ -83,101 +86,40 @@ const Home = (props) => {
                     </Toolbar>
                 </AppBar>
             </Box>
-            <div id="register-form" className='background center' style={{ display: "flex" }}>
+            <div>
 
                 <Typography variant="h2" color="primary" component="div">
                     Our Trips
                 </Typography>
 
                 {
-                    trips.map((trip, index) => {
+                    trips.map((trip) => {
                         return (
-                            <>
-                                <img
-                                    key={trip.tripID}
-                                    // src={trip.destination}
-                                    // src={trips[index].destination}
-                                    src={require(`../uploads/${trip.destination}.jpg`)}
-                                    // src={`${require}(../uploads/${trip.destination}.jpg).default`}
-                                    alt="error loading pic"
-                                />
-                                <Card sx={{ maxWidth: 345 }}
-                                    key={trip.tripID}
-                                >
-                                    <CardHeader
-                                        avatar={
-                                            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                                                R
-                                            </Avatar>
-                                        }
-                                        action={
-                                            <IconButton aria-label="settings">
-                                                <MoreVertIcon />
-                                            </IconButton>
-                                        }
-                                        title="Shrimp and Chorizo Paella"
-                                        subheader="September 14, 2016"
-                                    />
+                            <div className='' style={{ display: 'flex' }} key={trip.tripID}>
+
+                                <Card sx={{ maxWidth: 345 }}>
                                     <CardMedia
                                         component="img"
-                                        height="194"
-                                        image="/static/images/cards/paella.jpg"
-                                        alt="Paella dish"
+                                        alt="error loading picture"
+                                        height="140"
+                                        image={require(`../uploads/${trip.destination}.jpg`)}
                                     />
                                     <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            {trip.destination}
+                                        </Typography>
                                         <Typography variant="body2" color="text.secondary">
-                                            This impressive paella is a perfect party dish and a fun meal to cook
-                                            together with your guests. Add 1 cup of frozen peas along with the mussels,
-                                            if you like.
+                                            {trip.description}
                                         </Typography>
                                     </CardContent>
-                                    <CardActions disableSpacing>
-                                        <IconButton aria-label="add to favorites">
-                                            <FavoriteIcon />
-                                        </IconButton>
-                                        <IconButton aria-label="share">
-                                            <ShareIcon />
-                                        </IconButton>
-                                        {/* <ExpandMore
-                                            expand={expanded}
-                                            onClick={handleExpandClick}
-                                            aria-expanded={expanded}
-                                            aria-label="show more"
-                                        > */}
-                                        <ExpandMoreIcon />
-                                        {/* </ExpandMore> */}
+                                    <CardActions>
+                                        <Button size="small">Share</Button>
+                                        <Button size="small">Learn More</Button>
                                     </CardActions>
-                                    <Collapse in={expanded} timeout="auto" unmountOnExit>
-                                        <CardContent>
-                                            <Typography paragraph>Method:</Typography>
-                                            <Typography paragraph>
-                                                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-                                                aside for 10 minutes.
-                                            </Typography>
-                                            <Typography paragraph>
-                                                Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-                                                medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-                                                occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-                                                large plate and set aside, leaving chicken and chorizo in the pan. Add
-                                                pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-                                                stirring often until thickened and fragrant, about 10 minutes. Add
-                                                saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-                                            </Typography>
-                                            <Typography paragraph>
-                                                Add rice and stir very gently to distribute. Top with artichokes and
-                                                peppers, and cook without stirring, until most of the liquid is absorbed,
-                                                15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-                                                mussels, tucking them down into the rice, and cook again without
-                                                stirring, until mussels have opened and rice is just tender, 5 to 7
-                                                minutes more. (Discard any mussels that don&apos;t open.)
-                                            </Typography>
-                                            <Typography>
-                                                Set aside off of the heat to let rest for 10 minutes, and then serve.
-                                            </Typography>
-                                        </CardContent>
-                                    </Collapse>
                                 </Card>
-                            </>
+
+
+                            </div>
                         )
                     })
                 }
@@ -189,22 +131,15 @@ const Home = (props) => {
 
 export default Home;
 
-// trips.map((trip) => {
-//     // console.log(trip.destination)
-//     // let destination = trip.destination
-//     return (
-//         <>
-//             {/* <img src={require('./logo.jpeg')} /> */}
-//             < img
-//                 key={trip.tripID}
-//                 // src={trip.destination}
-//                 src={destination}
-
-//                 alt={"error loading"}
-//             />
-//         </>
-//     )
-// })
+{/* <img
+    key={trip.tripID}
+    // src={trip.destination}
+    // src={trips[index].destination}
+    style={{ height: 100, width: 100 }}
+    src={require(`../uploads/${trip.destination}.jpg`)}
+    // src={`${require}(../uploads/${trip.destination}.jpg).default`}
+    alt="error loading pic"
+/>  */}
 
 
 // import '../uploads/London.jpg';
@@ -229,3 +164,71 @@ export default Home;
 // import WashingtonDC from '../uploads/WashingtonDC.jpg'
 // import Tokyo from '../uploads/Tokyo.jpg'
 
+
+// {/* <Card sx={{ maxWidth: 345 }}
+// key={trip.tripID}
+// >
+// <CardHeader
+//     avatar={
+//         <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+//             {/* optional - make a random trip symbole */}
+//             {trip.destination.substring(1, 0)}
+//         </Avatar>
+//     }
+//     action={
+//         <IconButton aria-label="settings">
+//             <MoreVertIcon />
+//         </IconButton>
+//     }
+//     title={trip.destination}
+//     subheader={trip.date}
+// />
+// {/* <CardMedia
+//     component="img"
+//     height="194"
+//     // image={`${require}(../uploads/${trip.destination}.jpg).default`}
+//     image={require(`../uploads/${trip.destination}.jpg`)}
+//     alt="Paella dish"
+// /> */}
+// <img
+//     key={trip.tripID}
+//     // src={trip.destination}
+//     // src={trips[index].destination}
+//     src={require(`../uploads/${trip.destination}.jpg`)}
+//     // src={`${require}(../uploads/${trip.destination}.jpg).default`}
+//     alt="error loading pic"
+// />
+// <CardContent>
+//     <Typography variant="body2" color="text.secondary">
+//         This impressive paella is a perfect party dish and a fun meal to cook
+//         together with your guests. Add 1 cup of frozen peas along with the mussels,
+//         if you like.
+//     </Typography>
+// </CardContent>
+// <CardActions disableSpacing>
+//     <IconButton aria-label="add to favorites">
+//         <FavoriteIcon />
+//     </IconButton>
+//     <IconButton aria-label="share">
+//         <ShareIcon />
+//     </IconButton>
+//     <ExpandMore
+//         expand={expanded}
+//         onClick={handleExpandClick}
+//         aria-expanded={expanded}
+//         aria-label="show more"
+//     >
+//         <ExpandMoreIcon />
+//     </ExpandMore>
+// </CardActions>
+// <Collapse in={expanded} timeout="auto" unmountOnExit>
+//     <CardContent>
+//         <Typography paragraph>Method:</Typography>
+//         <Typography paragraph>
+
+//         </Typography>
+
+//     </CardContent>
+// </Collapse>
+// </Card>
+//  */}
